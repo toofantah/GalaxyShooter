11a0f2b778d6e5b3c5c49cf427086df0a2a39b56
toofantah <toofan.tah@gmail.com>
2021-06-16 02:20:11 +0300

    Update Player.cs

--- GalaxyShooter2D/Assets/Scripts/Player.cs
+++ GalaxyShooter2D/Assets/Scripts/Player.cs
@@ -19,21 +19,15 @@ public class Player : MonoBehaviour
 
     }
 
-    public void Movement()
+    void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float VerticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, VerticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        if (transform.position.y >= 2)
-        {
-            transform.position = new Vector3(transform.position.x, 2, 0);
-        }
-        else if (transform.position.y <= -3.5f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.5f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 1), 0);
+
         if (transform.position.x > 11)
         {
             transform.position = new Vector3(-11, transform.position.y, 0);
